f5fd8073ca850b8de9b4da037407c795fb715126
Sansruria <iSansruria@icloud.com>
2023-04-19 14:44:48 +0900

    Initial commit

---
+++ src/test/java/com/malorian/web/ExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.malorian.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ExampleApplicationTests {
+
+    @Test
+    void contextLoad() {
+
+    }
+}
