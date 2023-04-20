583082dc2fa0333f33851903569e03168207f870
Sansruria <iSansruria@icloud.com>
2023-04-20 21:45:14 +0900

    index.html에 list 제작중

---
+++ src/main/java/com/malorian/dto/Product.java
@@ -0,0 +1,12 @@
+package com.malorian.dto;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class Product {
+    private int seq;
+    private String category;
+    private String title;
+    private String name;
+    private int count;
+}
--- src/main/java/com/malorian/web/IndexController.java
+++ src/main/java/com/malorian/web/IndexController.java
@@ -1,15 +1,33 @@
 package com.malorian.web;
 
+import com.malorian.dto.Product;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
-    @GetMapping("/")
-    public String index() {
+//    @GetMapping("/")
+    @RequestMapping(value = "/", method = {RequestMethod.POST, RequestMethod.GET})
+    public String index(Model model) {
+        List<Product> productList = new ArrayList<>();
+
+        productList.add(new Product(1,"정보","드디어 첫 게시글을 작성해 보아요","말로리안",1));
+        productList.add(new Product(2,"정보","드디어 첫 게시글을 작성해 보아요","말로리안",1));
+        productList.add(new Product(3,"정보","드디어 첫 게시글을 작성해 보아요","말로리안",1));
+        productList.add(new Product(4,"정보","드디어 첫 게시글을 작성해 보아요","말로리안",1));
+        productList.add(new Product(5,"정보","드디어 첫 게시글을 작성해 보아요","말로리안",1));
+
+        model.addAttribute("productList", productList);
+
         return "views/index";
     }
 
